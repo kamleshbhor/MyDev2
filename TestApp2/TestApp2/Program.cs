8425a1c0e1e552cefda87e3e57a9e398d0029cd0
Kamlesh.Bhor <Kamlesh.Bhor@SAGITEC-1853>
2017-01-06 14:34:44 +0530

    Initial commit

---
+++ TestApp2/TestApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetName();
+        }
+
+        public static void GetName()
+        {
+            Console.WriteLine("Sagitec");
+        }
+    }
+}
